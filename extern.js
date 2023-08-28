e23ef864e2f70c0f60f26c73cd8eb037431a6309
RaupeCHR <raupechr@gmail.com>
2023-08-28 11:02:44 +0200

    bearbeiten der extern.js Datei

--- extern.js
+++ extern.js
@@ -1,3 +1,9 @@
 let test = "hello " + "world"
 console.log(test)
 console.log(typeof(test))
+
+function helloWorld(arg1, arg2) {                                               //Eine Funktion mit gesetzten Argumenten(arg)
+    console.log("arg1", arg1)
+    console.log("arg2", arg2)
+
+}
