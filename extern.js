cd608c47a67ccf27bf6dfb3e665bc300047a3aa6
RaupeCHR <raupechr@gmail.com>
2023-08-28 11:13:53 +0200

    bearbeiten der extern.js Datei

--- extern.js
+++ extern.js
@@ -2,8 +2,22 @@ let test = "hello " + "world"
 console.log(test)
 console.log(typeof(test))
 
-function helloWorld(arg1, arg2) {                                               //Eine Funktion mit gesetzten Argumenten(arg)
-    console.log("arg1", arg1)
-    console.log("arg2", arg2)
+// (param1, param2, param3)
+
+function helloWorld(pram1, param2, param3) {                                               //Eine Funktion mit gesetzten Paramentern
+    //console.log(arguments)
+    console.log("param1", param1)
+    console.log("param2", param2)
+    console.log("param3", param3)
 
 }
+
+// args
+console.log("first function invocation", 2, "third function invocation")                    // Funtionen die mit Argumenten auf die Parameter zugreift
+helloWorld("hello", "world")
+
+console.log("second function ivocation")
+helloWorld("hello", 1)
+
+console.log("third function invocation")
+helloWolrd("hello", true, 3)
