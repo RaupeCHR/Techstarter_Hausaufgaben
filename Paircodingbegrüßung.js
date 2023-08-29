ca03fa2e400af49ef8b23b6d2dc3b2e7cc74d23d
RaupeCHR <raupechr@gmail.com>
2023-08-29 09:57:33 +0200

    erstellen der Paircodingbegrüßung.js

---
+++ Paircodingbegrüßung.js
@@ -0,0 +1,11 @@
+// moch data
+let name = ""
+//let input = ""
+//let result = "Hello " + name
+
+function greeting(name) {
+    return "Hey " + name
+}
+
+const result = greeting("Pablo")
+console.log(result)
