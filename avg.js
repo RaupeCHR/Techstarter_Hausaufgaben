7cb3f2d530f01c5998cc38d358520e305ef1370a
RaupeCHR <raupechr@gmail.com>
2023-08-29 11:16:12 +0200

    erstellen der avg.js

---
+++ avg.js
@@ -0,0 +1,15 @@
+
+
+function average(numbers) {
+    let sum = 0;
+    for (let numer of numbers) {
+        sum += numbers;
+    }
+    return sum / numbers.length
+}
+
+let numbers = [1,2,3,4,5]
+
+let result = average(numbers)
+
+console.log(result)
