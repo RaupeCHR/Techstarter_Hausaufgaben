0ba270040dc299b0ccfd877bbe78f574784fc131
RaupeCHR <raupechr@gmail.com>
2023-09-19 13:47:33 +0200

    bearbeiten der Hausaufgabe_19_09

--- Hausaufgabe_19_09/index.js
+++ Hausaufgabe_19_09/index.js
@@ -1,12 +1,22 @@
 import express from "express"
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+
 
 app.get('/', (req, res) => {
-    res.send('this ist my homework on 19th of september 2023!');
-  });
-  
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-  });
+  res.send('Herzlich Willkommen zu den Hausaufgaben von Christoph Raupach!');
+});
+
+app.get('/oldhtml', (req, res) => {
+    res.sendFile('todo.html', { root: './'});
+});
+
+app.get('/cat/says:', (req, res) => {
+  const text = req.params.says;
+  res.redirect(`https://cataas.com/cat/says/${text}`);
+});
+
+app.listen(port, () => {
+  console.log(`Express-Server hört auf Port ${port}`);
+});
