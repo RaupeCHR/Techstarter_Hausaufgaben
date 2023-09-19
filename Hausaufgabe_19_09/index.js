d4f0e3c939ae6b47700e83869a417c6d01ec9e16
RaupeCHR <raupechr@gmail.com>
2023-09-19 15:50:04 +0200

    ExpressServerSpielerei ;)

--- Hausaufgabe_19_09/index.js
+++ Hausaufgabe_19_09/index.js
@@ -1,23 +1,53 @@
 import express from "express"
+import bodyParser from "body-parser"
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
+app.use(bodyParser.urlencoded({ extended: true }));
 
+const form = `
+<h1>Herzlich Willkommen zur ExpressServerSpielerei von RaupeCHR!</h1>
+<form method="post" action="/login">
+    <label for="name">Name:</label>
+    <input name="name" type="text">
+    <label for="pw">Passwort:</label>
+    <input name="pw" type="password">
+    <button type="submit">Login</button>
+</form>
+</br>
+<a href="/">Home</a>
+<a href="/oldhtml">To Do List</a>
+<a href="/cat/:says">Catpictures</a>
+          `
+
+const user = [
+    {name: "Christoph", passwort: "123"},
+    {name: "Bartek", passwort: "456"},
+    {name: "Evelyn", passwort: "789"}
+]
 
 app.get('/', (req, res) => {
-  res.send(`
-  <h1>Herzlich Willkommen zu den Hausaufgaben von Christoph Raupach!</h1>
-  <a href="/">Home</a>
-  <a href="/oldhtml">To Do List</a>
-  <a href="/cat/:says">Catpictures</a>
-            `);
+  res.send(form);
 });
+
+app.post("/login", (req, res) => {
+    const {name, pw}  = req.body
+    
+    for  (let i = 0; i < user.length; i++){
+      if (name === user[i].name && pw === user[i].passwort){
+        res.send(`login erfolgreich`)
+        return
+      }
+    }
+
+    res.send(`Login fehlgeschlagen`)
+})
 
 app.get('/oldhtml', (req, res) => {
     res.sendFile('todo.html', { root: './'});
 });
 
-app.get('/cat/says:', (req, res) => {
+app.get('/cat/:says', (req, res) => {
   const text = req.params.says;
   res.redirect(`https://cataas.com/cat/says/${text}`);
 });
