4cd4e374f0658f8b72677c696fe7fde240c1c884
RaupeCHR <raupechr@gmail.com>
2023-09-19 12:46:42 +0200

    bearbeiten der Hausaufgabe_19_09

--- Hausaufgabe_19_09/index.js
+++ Hausaufgabe_19_09/index.js
@@ -1,1 +1,12 @@
-console.log("Hausaufgabe");
+import express from "express"
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('this ist my homework on 19th of september 2023!');
+  });
+  
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
